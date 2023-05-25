27064fc717f3d262dc53ca58c72f93c40306d361
Bryan <bryanferrando59@gmail.com>
2023-05-25 23:18:13 +0200

    Add nelmio/cors-bundle

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+$request = Request::createFromGlobals();
+
+$request->headers->set('Access-Control-Allow-Origin', '*');
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
